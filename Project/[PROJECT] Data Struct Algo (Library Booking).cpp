95477f6a044f2f35667ef8fa35af6c1d48c032ae
Arif <111127538+SNOW15782@users.noreply.github.com>
2023-05-26 11:05:45 +0800

    updated

--- Project/[PROJECT] Data Struct Algo (Library Booking).cpp
+++ Project/[PROJECT] Data Struct Algo (Library Booking).cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+//BOOK STRUCTURE 
+// TITLE, ISBN, ID, AVAILABLE (BOOL)
+struct Book {
+	string id;
+	int isbn; 
+	bool available; 
+	string title; 
+	//linked list 
+	Book* next; 
+	
+};
+
+//ROOM STRUCTURE 
+//NAME, CAPACITY, AVAILABLE (BOOL)
+struct Room{
+	string name; 
+	bool available; 
+	int capacity; 
+	//linked list 
+	Room* next;
+	
+};
+
+
 
 int main() {
     string name, id, password;
@@ -25,13 +51,43 @@ int main() {
     getline(cin, password);
 
     // Check both ID and password
-    if (id == "correctID" || password == "123456") {
-        cout << "Login Successful! Welcome, " << name << ".\n";
-    } else {
+    if (id == "1211206128" || password == "123456") {
+        cout << "Login Successful! Welcome, " << name << ".\n\n";
+        system ("cls"); 
+		string choice; 
+		
+		while (true){
+			cout << "Hi "<< name<< " Welcome to do?\n";
+			cout << "1. Book a Room\n";
+			cout << "2. Rent a Book\n";
+			cout << "3. Timetable\n";
+			cout << "4. Exit\n"; 
+			cout << "Enter your choice: "; 
+			cin >> choice; 
+			
+			//IF ELSE SELECTION 
+			if (choice == "1"){
+				//FUNCTION BOOK A ROOM
+			}
+			else if (choice == "2"){
+				//FUNCTION RENT A BOOK
+			}
+			else if (choice == "3"){
+				//FUCNTION TIMETABLE 
+			}
+			else if (choice == "4"){
+				cout << "Goodbye!"<<endl;
+				break; 
+			}
+			else{
+				cout << "Invalid choice. Please try again" <<endl;
+			}
+		} 
+	
+	} else {
         cout << "Incorrect ID or Password. Please try again.\n";
         exit(0);
     }
 
     return 0;
 }
-
