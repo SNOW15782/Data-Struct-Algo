c7a904503400878c28445716612aa902b15b38e8
Arif <111127538+SNOW15782@users.noreply.github.com>
2023-05-29 23:42:20 +0800

    Update 29/5/2023
    - Add choices when user enter the book section
    - Searching algo not working atm, later need fix -Arif

--- Project/[PROJECT] Data Struct Algo (Library Booking).cpp
+++ Project/[PROJECT] Data Struct Algo (Library Booking).cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stack>
 using namespace std;
 
 //BOOK STRUCTURE 
@@ -43,8 +44,36 @@ class BookList{
 				return current; 
 			}
 			//ELSE, RETURN NULLPTR
-			return nullptr;
+			current = current->next;
 		}
+		return nullptr;
+	}
+	//SEARCH FOR BOOK BY TITLE
+	Book* searchBookByTitle(const string& title){
+		Book* current = head;
+		//SEARCHING ALGORITHM
+		while (current != nullptr){
+			if (current ->id == title){
+				return current; 
+			}
+			//ELSE, RETURN NULLPTR
+			current = current->next;
+		}
+			return nullptr;
+	}
+	
+	//SEARCH FOR BOOK BY ISBN
+	Book* searchBookByISBN(const string& isbn){
+		Book* current = head;
+		//SEARCHING ALGORITHM
+		while (current != nullptr){
+			if (current ->id == isbn){
+				return current; 
+			}
+			//ELSE, RETURN NULLPTR
+			current = current->next;
+		}
+			return nullptr;
 	}
 	
 	//FUNCTION TO ADD A BOOK TO THE DATABASE, (REFER BOOK STRUCTURE), STORE ID, ISBN, TITLE, QUANTITY
@@ -68,13 +97,14 @@ class BookList{
 	else{
 		Book* current = head;
 		while (current->next != nullptr){
-			current->next = newBook; 
+			current = current ->next; 
 		}
+		current->next = newBook; 
 	}
 }
-
-//FUNCTION TO RESERVE A BOOK 
-void reserveBook (const string& id){
+}; 
+//FUNCTION TO RENT A BOOK 
+/*void reserveBook (const string& id){
 	Book* book = searchBookByID (id);
 
 	if (book == nullptr){
@@ -84,14 +114,17 @@ void reserveBook (const string& id){
 	
 	if (!book->available){
 		cout << "Book is not avaialble."<<endl;
-		//ADD CODE TO SHOW THE NEXT AVAIABLE DATE 
+		
+		
 		return; 
 	}
 	
 	if (book->quantity > 0){
 		cout << "Book Found"<< book->title <<endl; 
+		cout << "Would you like to reserve this book? (Yes/No):";
 		//declare string reservation
 		string reservationOption; 
+		cin >> reservationOption;
 		
 		if (reservationOption == "Yes"){
 			string name; 
@@ -112,10 +145,28 @@ void reserveBook (const string& id){
 		cout <<"Book is out of stock"<<endl;
 		//Add code showing what book available 
 	}
-}
+}*/
+
+class MeetingRoomList{
+	private: 
+	Room*head; 
+	
+	public: 
+	MeetingRoomList():head(nullptr){}
+	
+	//FUNCTION TO SEARCH FOR A ROOM BY NAME
+	Room* searchRoomByName (const string& name){
+		Room* current = head; 
+		while (current!= nullptr){
+			if (current->name == name){
+				return current;
+			}
+			current = current->next;
+		}
+		return nullptr;
+	}
+//FUCNTION TO ADD A ROOM TO THE DATABASE (DANIEL PART) 
 };
-
-
 int main() {
     string name, id, password;
 
@@ -154,6 +205,9 @@ int main() {
         BookList.addBook ("5","ISBN5 ", "Book 5", 2);
         
 		//Create Meeting room list 
+		MeetingRoomList roomList;
+		// NAME OF THE ROOM, CAPACITY, LATER ADD
+		
 		
 		string choice; 
 		
@@ -165,13 +219,76 @@ int main() {
 			cout << "4. Exit\n"; 
 			cout << "Enter your choice: "; 
 			cin >> choice; 
+			system ("cls");
 			
 			//IF ELSE SELECTION 
-			if (choice == "1"){
+			if (choice == "1"){ 
+				string roomChoice; 
 				//FUNCTION BOOK A ROOM
 			}
 			else if (choice == "2"){
-				//FUNCTION RENT A BOOK
+				//BOOK OPTIONS GOES HERE
+				string bookChoice;
+				cout << "What would you like to do?\n"; 
+				cout << "1. Search for a Book by Title\n";
+				cout << "2. Search for a Book by ISBN\n";
+				cout << "3. Search for a Book by ID\n"; 
+				cout << "4. Go back to the main menu\n";
+				cout << "Enter your choice: "; 
+				cin >> bookChoice; 
+				
+				//IF ELSE 
+				//SEARCHING PART MIGHT NOT WORK ATM (LATER FIX) - ARIF
+				if (bookChoice == "1"){
+					string title; 
+					cout << "Enter the title of the book: "; 
+					cin.ignore();
+					getline (cin, title); 
+					
+					//FUNCTION SEARCHING BOOK TITLE 
+					Book* book = BookList.searchBookByTitle(title);
+					if (book != nullptr){
+						cout << "Book Found: "<<book->title <<endl; 
+					}
+					else{
+						cout  << "Book not found" <<endl; 
+					}
+				}
+				else if (bookChoice == "2"){
+					string isbn;
+					cout << "Enter the ISBN of the book: ";
+					cin.ignore();
+					getline(cin, isbn);
+					
+					Book* book = BookList.searchBookByISBN(isbn);
+					if (book != nullptr){
+						cout << "Book Found: "<<book->title <<endl; 
+					}
+					else{
+						cout  << "Book not found" <<endl; 
+					}
+				}
+				else if (bookChoice == "3"){
+					string isbn;
+					cout << "Enter the ID of the book: ";
+					cin.ignore();
+					getline(cin, id);
+					
+					Book* book = BookList.searchBookByID(id);
+					if (book != nullptr){
+						cout << "Book Found: "<<book->title <<endl; 
+					}
+					else{
+						cout  << "Book not found" <<endl; 
+					}	
+				}
+				else if (bookChoice == "4")
+				//GO BACK TO MAIN MENU 
+				continue; 
+				else {
+					cout << "Invalid choice. Please try again." <<endl; 
+					system ("cls"); 
+				}
 			}
 			else if (choice == "3"){
 				//FUCNTION TIMETABLE 
